786356d697654f0eaeca4bf98b4fe3a9d8e487c6
rui-watanabe <wataru610806@gmail.com>
2019-07-24 17:41:39 +0900

    users-action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,24 @@ class UsersController < ApplicationController
    # 定義しないと、Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
    #現在ログインしているユーザーidがの投稿しているツイートを全て持ってくる
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+  
  end
  
  
