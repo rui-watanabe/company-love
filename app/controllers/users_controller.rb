867220487fa5eec3791362dccf1f140ef70e1f20
rui-watanabe <wataru610806@gmail.com>
2019-07-25 12:22:47 +0900

    profile-column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
    # アソシエーションを定義したことによって使える
    # 定義しないと、Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
    #現在ログインしているユーザーidがの投稿しているツイートを全て持ってくる
+   
   end
 
   def edit
